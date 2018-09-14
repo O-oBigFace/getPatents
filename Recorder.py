26474d60ff002fb04609631fc51c9ec33de2647b
John <jiangh_wu@163.com>
2018-09-14 15:44:57 +0800

    record文件一定要放入results文件夹中

--- Recorder.py
+++ Recorder.py
@@ -39,3 +39,25 @@ def is_Exist():
         with open(path_record_all, "w", encoding="utf-8") as all:
             all.write("{}")
 
+
+#  汇总所有record
+#  步骤：将左右的record文件放入record文件夹中，运行此函数
+def all_record():
+    with open(os.path.join(os.getcwd(), 'results', "record"), 'r', encoding="utf-8") as rf:
+        a_record = json.loads(rf.read())
+
+    for record_name in os.listdir(os.path.join(os.getcwd(), "record")):
+        path = os.path.join(os.getcwd(), 'record', record_name)
+        print(path)
+        with open(path, 'r', encoding='utf-8') as f:
+            tb = json.loads(f.read())
+            for key, value in tb.items():
+                a_record[key] = value
+    print(len(a_record))
+    with open(os.path.join(os.getcwd(), 'results', "record"), 'w', encoding="utf-8") as wf:
+        wf.write(json.dumps(a_record))
+
+
+if __name__ == '__main__':
+    all_record()
+
